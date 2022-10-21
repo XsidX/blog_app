62d98892e066d4ff4aeefd261fa301bdcbc80409
XsidX <developer.sid@outlook.com>
2022-10-21 12:21:55 +0300

    Create Comment model and define it's associations, custom method; update_comments_count

---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :update_comments_count
+
+  private
+
+  def update_comments_count
+    post.increment!(:comments_count)
+  end
+end
