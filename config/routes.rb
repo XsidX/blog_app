b16e392dedcfba62b95cef3dfcac129c77759b2b
XsidX <developer.sid@outlook.com>
2022-10-24 12:41:01 +0300

    Restrict routes to only include index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   # Define route for users in association with posts
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
 end
