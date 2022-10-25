4fd080a9219c850476103cd7338f0a2218a7fd53
XsidX <developer.sid@outlook.com>
2022-10-25 14:07:59 +0300

    Add html template and styling for post index and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
+  before_action :set_user, only: %i[index show]
   def index
-    @posts = Post.all
+    @posts = @user.posts
   end
 
   def show; end
@@ -10,4 +11,9 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
