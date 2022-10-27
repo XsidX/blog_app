b25a97576eeed9c82cad2a566713d63e72d27d99
XsidX <developer.sid@outlook.com>
2022-10-27 09:01:55 +0300

    Create js/stimulus methods to toggle dropdown menus

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
-  static targets = [ "openMyMenu", "openUserDropdown" ]
+  static targets = [ "openMyMenu", "openUserDropdown", "openPostDropdown" ]
   connect() {
     this.openMyMenuTarget.addEventListener('click', this.toggleMyDropdownMenu)
     this.openUserDropdownTarget.addEventListener('click', this.toggleUserDropdownMenu)
+    this.openPostDropdownTarget.addEventListener('click', this.togglePostDropdownMenu)
   }
 
   toggleMyDropdownMenu() {
@@ -15,4 +16,9 @@ export default class extends Controller {
   toggleUserDropdownMenu() {
     toggle(document.getElementById('user-dropdown-menu'))
   }
+
+  togglePostDropdownMenu() {
+    toggle(document.getElementById('post-dropdown-menu'))
+  }
+
 }
