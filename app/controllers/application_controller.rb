84fc3134471c3748bbb72fb07c2fd8ea8b08d593
XsidX <developer.sid@outlook.com>
2022-10-29 00:18:51 +0300

    fix temp user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user
     temp_user = User.new(name: 'Brad', photo: 'https://randomuser.me/api/portraits/men/65.jpg', bio: 'Brad has worked in the technology and association space for more than 15 years now. He is the CEO and President of MemberClicks, an all-in-one membership management software company, and has helped them grow from just under 300 customers in 2004 to 3,000 today across North America.')
-    @current_user = User.all.find(1) || temp_user
+    @current_user = temp_user
   end
 end
