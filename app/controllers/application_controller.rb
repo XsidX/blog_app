fd350513ae424c9a63d0ee9c527e0fa710dab6cb
XsidX <developer.sid@outlook.com>
2022-11-02 17:27:33 +0300

    Configure devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pagy::Backend
-  def current_user
-    @current_user = User.all.find(1)
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
   end
+
 end
