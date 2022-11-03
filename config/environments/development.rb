24c4f1d794d0e0c12af6ba03cfd2fca16eaa334b
XsidX <developer.sid@outlook.com>
2022-11-03 19:20:40 +0300

    Configure mail catcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 3000,
+    domain: "gmail.com",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
